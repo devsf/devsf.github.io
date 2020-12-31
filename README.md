a3cc1cd97f8e902a67da7c8626606008a781fba6
Sergio Ferraresi <ferraresi@meeo.it>
2020-12-31 12:22:53 +0100

    Changelog 2020-12-31+dev001

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # sergioferraresi.it source code
+
+## Changelog
+
+### 2020-12-31+dev001
+*  moved from [Bootstratp v5.0.0-beta1](https://getbootstrap.com/) to [Bulma v0.9.1](https://bulma.io/)
+*  added `script.js` to manage row buttons
+*  text improvements
+
+### 2020-12-24+dev001
+*  updated to [Bootstratp v5.0.0-beta1](https://getbootstrap.com/)
+*  UI improbements
---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var lastScrollTop = 0;
+function onDown() {
+    window.scrollBy({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+    });
+    document.querySelector('div.icon-down-wrapper > a').classList.add('is-hidden');
+    document.querySelector('div.icon-up-wrapper > a').classList.remove('is-hidden');
+};
+function onUp() {
+    window.scrollBy({
+        top: -document.body.scrollHeight,
+        behavior: 'smooth'
+    });
+    document.querySelector('div.icon-up-wrapper > a').classList.add('is-hidden');
+    document.querySelector('div.icon-down-wrapper > a').classList.remove('is-hidden');
+};
+window.onscroll = function (ev) {
+    var st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            onDown();
+        }
+    } else {
+        if (document.body.scrollTop === 0) {
+            onUp();
+        }
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+};
